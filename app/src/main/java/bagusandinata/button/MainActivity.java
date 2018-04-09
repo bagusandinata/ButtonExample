51c88fb8b4465f91426cf1fd7f8dd3831e98ad0f
bagus anugerah dinata <bagus.anugerah.dinata@gmail.com>
2018-04-09 16:52:39 +0700

    flat and floating button

--- app/src/main/java/bagusandinata/button/MainActivity.java
+++ app/src/main/java/bagusandinata/button/MainActivity.java
@@ -1,5 +1,6 @@
 package bagusandinata.button;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
@@ -9,6 +10,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     AppCompatButton raisedButton;
+    AppCompatButton flatButton;
+    FloatingActionButton fabButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +19,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         raisedButton = findViewById(R.id.raised_btn);
+        flatButton = findViewById(R.id.flat_btn);
+        fabButton = findViewById(R.id.fab_btn);
 
         raisedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Raised Button", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        flatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Flat Button", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        fabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Fab Button", Toast.LENGTH_SHORT).show();
             }
         });
     }
